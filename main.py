1786f0f09de8f99bc3ef45f379130e367f48d322
Drayneur <50057580+DrayNeur@users.noreply.github.com>
2022-04-13 19:42:36 +0200

    Update main.py
    tiens bebou baka j'ai fix ton code qui meme si ce n'est qu'un test pue la merde, preuve que tu ne sais pas coder, aucune convention n'est respectée

--- main.py
+++ main.py
@@ -3,61 +3,63 @@ import json
 from colorama import Fore
 import os
 
-# User data
-
-pseudo = 'Hisoka'
-rank = 'admin'
-id = '1'
-ban = False
-
-# Token AND Chat ID
-
-bot1 = {
-    'token':'2134902146:AAHgCC3vkOatqQaMQuDo5l0h3NxCITHDUl4',
-    'chat_id':'-1001648468501'
+# User data (pas de constant parceque c'est dynamique)
+user_data = {
+    'username': 'Hisokaleskid', # variable name are in english :)
+    'rank': 'admin',
+    'id': 1,
+    'banned': False
 }
 
-bot2 = {
-    'token':'2106470373:AAGDKSr7fD02dI17NqMyjSAdvPAeJljns44',
-    'chat_id':'-1001648468501'
-}
+# Token AND Chat ID (contant :D)
+BOTS = [
+    {
+        'token':'2134902146:AAHgCC3vkOatqQaMQuDo5l0h3NxCITHDUl4',
+        'chat_id':'-1001648468501'
+    },
+    {
+        'token':'2106470373:AAGDKSr7fD02dI17NqMyjSAdvPAeJljns44',
+        'chat_id':'-1001648468501'
+    }
+]
 
 def get_message():
-    url_get = 'https://api.telegram.org/bot' + bot1['token'] + '/getUpdates'
+    url_get = 'https://api.telegram.org/bot' + BOTS[0]['token'] + '/getUpdates'
     req = requests.get(url_get).json()
     if req['ok'] == True:
         data_message = req['result']
         for message in data_message:
             try:
                 message = json.loads(message['message']['text'])
-                rank = message['rank']
-                pseudo = message['pseudo']
+                user_data['rank'] = message['rank']
+                user_data['username'] = message['pseudo']
+                user_data['id'] = message['id']
                 msg = message['content']
-                id = message['id']
+                
                 if rank == "admin":
-                    prefix = f'{Fore.LIGHTRED_EX}[ADMIN] {Fore.GREEN} {pseudo} [{id}] : {Fore.WHITE}' + msg
+                    prefix = f'{Fore.LIGHTRED_EX}[ADMIN] {Fore.GREEN} {user_data['username']} [{user_data['id']}] : {Fore.WHITE}' + msg
                 elif rank == "visitor":
-                    prefix = f'{Fore.LIGHTCYAN_EX}[VISITOR] {Fore.GREEN} {pseudo} [{id}] : {Fore.WHITE}' + msg
+                    prefix = f'{Fore.LIGHTCYAN_EX}[VISITOR] {Fore.GREEN} {user_data['username']} [{user_data['id']}] : {Fore.WHITE}' + msg
                 else:
-                    prefix = f'{Fore.LIGHTYELLOW_EX}[UNKNOW] {Fore.GREEN} {pseudo} [{id}] : {Fore.WHITE}' + msg
+                    prefix = f'{Fore.LIGHTYELLOW_EX}[UNKNOW] {Fore.GREEN} {user_data['username']} [{user_data['id']}] : {Fore.WHITE}' + msg
                 print(prefix)
             except:
                 pass
 
 def send_message(message):
-    msg = {'id':id,'pseudo':pseudo,'rank':rank,'content':message}
-    url_post = f"https://api.telegram.org/bot{bot2['token']}/sendMessage?chat_id={bot1['chat_id']}&text={msg}"
+    msg = {'id':id,'pseudo':user_data['username'],'rank':user_data['rank'],'content':message}
+    url_post = f"https://api.telegram.org/bot{BOTS[1]['token']}/sendMessage?chat_id={BOTS[0]['chat_id']}&text={msg}"
     req = requests.get(url_post).json()
     print(req)
 
 # Check rank
 
-    
-if ban != True:
-    os.system('cls')
-    print('Start chatting with username : ' + pseudo + ' and rank : ' + rank + '\n')
-    send_message(input())
-    get_message()
+if __name__ == '__main__':
+    if user_data['banned'] != True:
+        os.system('cls')
+        print('Start chatting with username : ' + user_data['username'] + ' and rank : ' + user_data['rank'] + '\n')
+        send_message(input())
+        get_message()
     
     
     
